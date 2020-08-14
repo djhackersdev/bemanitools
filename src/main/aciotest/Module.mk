837fa049aafae6ae765e3843d77684e84eca4824
Will Xyen <willxinc@users.noreply.github.com>
2020-08-14 06:25:27 -0700

    sdvxio-bio2: Add sdvxio BIO2 along with bio2drv and aciotest updates

--- src/main/aciotest/Module.mk
+++ src/main/aciotest/Module.mk
@@ -1,10 +1,12 @@
 exes            += aciotest
 
 libs_aciotest     := \
+    bio2drv \
     aciodrv \
     util \
 
 src_aciotest      := \
     icca.c \
     kfca.c \
+    bi2a-sdvx.c \
     main.c \
---
+++ src/main/bio2drv/Module.mk
@@ -0,0 +1,9 @@
+libs            += bio2drv
+
+libs_bio2drv     := \
+    aciodrv
+
+src_bio2drv     := \
+    detect.c \
+    config-bio2.c \
+    bi2a-sdvx.c \
