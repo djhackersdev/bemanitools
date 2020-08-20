1d25abc10b5db49cf4a86b66e36b955d7a09f21a
icex2 <djh.icex2@gmail.com>
2020-08-20 11:12:46 +0200

    util: Add signal module introducing signal and exception handling
    Right now, this prints to the console using the util/log logger
    only. Stacktrace on exception still needs to be added.

---
+++ src/main/util/signal.h
@@ -0,0 +1,20 @@
+#pragma once
+
+/**
+ * Function signature of shutdown handler.
+ */
+typedef void (*signal_shutdown_handler_t)();
+
+/**
+ * Initialize signal and exception handlers. Exceptions and signals, are logged using the logger
+ * to create visibility.
+ */
+void signal_exception_handler_init();
+
+/**
+ * Register a handler function that gets called when the application is supposed to shut down,
+ * e.g. on CTRL + C.
+ *
+ * @param handler Handler function to register.
+ */
+void signal_register_shutdown_handler(signal_shutdown_handler_t handler);
