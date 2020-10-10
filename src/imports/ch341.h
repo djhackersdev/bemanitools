f39843b41f62e4e81fe3244a3bdf4d18a3aa116b
Will Toohey <will@mon.im>
2020-10-10 22:06:58 +1000

    Better comments in ch341.h

--- src/imports/ch341.h
+++ src/imports/ch341.h
@@ -1,8 +1,15 @@
+/**
+ * This is the header for ch341dll.dll, a library for interfacing with the
+ * CH341 USB to serial/parallel port chip. Magic Box uses this for their
+ * proprietary IO, and it provides access to the panel sensors.
+ *
+ * The full .h for this file is floating around online with many more functions
+ * and comments (in Chinese). Only the bare minimum definitions are provided
+ * here for use in jbio-magicbox
+ */
+
 #ifndef IMPORTS_CH341_H
 #define IMPORTS_CH341_H
-
-// The bare minimum definitions to implement jbio-magicbox
-// There is a full .h for this DLL floating around online, if you care
 
 HANDLE WINAPI CH341OpenDevice(ULONG iIndex);
 VOID WINAPI CH341CloseDevice(ULONG iIndex);
