0c88518a1f879e4426f69feb05e64589aeeb56c8
icex2 <djh.icex2@gmail.com>
2020-12-16 23:10:13 +0100

    bio2: Add BIO2_BI2A_CMD_INIT command
    Turns out this is different from the generic CLEAR command used
    on other ACIO devices. Has further implications when initializing
    BIO2 boards for specific games, e.g. IIDX.

--- src/main/bio2/bio2.h
+++ src/main/bio2/bio2.h
@@ -6,6 +6,9 @@
 #include <stdint.h>
 
 enum bio2_bi2a_cmd {
+    // Same as AC_IO_CMD_CLEAR but called differently
+    // with a parameter required to initialize the IO correctly
+    BIO2_BI2A_CMD_INIT = 0x0100,
     BIO2_BI2A_CMD_WATCHDOG = 0x0120,
     BIO2_BI2A_CMD_POLL = 0x0152, // For IIDX
 };
