1b31f56485d046128bce77cc2565d89fc8a6aaba
icex2 <djh.icex2@gmail.com>
2020-12-31 15:55:56 +0100

    vigem-iidxio: Refactor configuration
    Add more properties from Grim's implementation to support the
    additional features. Re-structure struct to be less repetitive
    on variable names.

--- src/main/vigem-iidxio/config.h
+++ src/main/vigem-iidxio/config.h
@@ -6,11 +6,26 @@
 #include "cconfig/cconfig.h"
 
 struct vigem_iidxio_config {
-    bool enable_keylight;
-    bool relative_analog;
-    int32_t cab_light_mode;
-    char text_16seg[1024 + 1];
-    int32_t text_scroll_cycle_time_ms;
+    struct tt {
+        struct analog {
+            bool relative;
+            int32_t relative_sensitivity;
+        } analog;
+
+        struct button {
+            int32_t debounce;
+            int32_t threshold;
+        } button;
+
+        bool debug_output;
+    } tt;
+    
+    struct cab_light {
+        bool enable_keylight;
+        int32_t light_mode;
+        char text_16seg[1024 + 1];
+        int32_t text_scroll_cycle_time_ms;
+    } cab_light;
 };
 
 bool vigem_iidxio_config_get(struct vigem_iidxio_config *config_out);
