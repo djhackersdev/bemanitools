fa286fc5e656b971b2469911d2cc831c4bc45c7b
icex2 <djh.icex2@gmail.com>
2021-06-26 11:18:08 +0200

    Refine CI/CD pipeline
    Make use of GitLab features like the package registry to store dist
    packages, GitLab releases and broadcast the release to different
    channels.
    
    Overall, this enables developers to be push out releases for frequently
    since the amount of manual work is reduced.

---
+++ scripts/ci/create-release-message.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+VERSION="$1"
+
+section_active=""
+changelog_excerpt=""
+
+while IFS= read -r line; do
+    if [ "$section_active" ]; then
+        if [[ "$(echo "$line" | grep '^#')" ]]; then
+            section_active=""
+        else
+            changelog_excerpt="$(printf "%s\n%s" "$changelog_excerpt" "$line")"
+        fi
+    else
+        if [ "$line" = "## $VERSION" ]; then
+            section_active="1"
+        fi
+    fi
+done
+
+if [ "$changelog_excerpt" ]; then
+    printf "%s" "$changelog_excerpt"
+    exit 0
+else
+    echo "Could not find version in changelog: $VERSION"
+    exit 1
+fi
