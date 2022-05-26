0586ea50aaee2ea6d57e71cc0abf882b9191dce8
QCDLZCLW3K <1329-33c17f40@users.noreply.dev.s-ul.net>
2022-05-26 21:47:06 +0900

    Add ddr-12 EU support

---
+++ dist/ddr/gamestart-12-eu.bat
@@ -0,0 +1,11 @@
+@echo off
+
+cd /d %~dp0
+
+if not exist conf\nvram mkdir conf\nvram
+if not exist conf\raw mkdir conf\raw
+
+regsvr32 /s com\CLVsd.ax
+regsvr32 /s com\xactengine2_10.dll
+
+inject ddrhook1.dll DDR.exe --config ddr-12-eu.conf %*
