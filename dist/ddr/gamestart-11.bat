885de752f00583abca5b310489f22584e0e41d69
QCDLZCLW3K <1329-33c17f40@users.noreply.dev.s-ul.net>
2022-05-26 20:43:41 +0900

    Add support for DDR X2 US

--- dist/ddr/gamestart-11.bat
+++ dist/ddr/gamestart-11.bat
@@ -2,10 +2,10 @@
 
 cd /d %~dp0
 
-if not exist conf mkdir conf
 if not exist conf\nvram mkdir conf\nvram
 if not exist conf\raw mkdir conf\raw
 
-regsvr32 /s xactengine2_10.dll
+regsvr32 /s com\CLVsd.ax
+regsvr32 /s com\xactengine2_10.dll
 
 inject ddrhook1.dll DDR.exe --config ddr-11.conf %*
--- dist/ddr/gamestart-12-us.bat
+++ dist/ddr/gamestart-12-us.bat
@@ -8,4 +8,4 @@ if not exist conf\raw mkdir conf\raw
 regsvr32 /s com\CLVsd.ax
 regsvr32 /s com\xactengine2_10.dll
 
-inject ddrhook1.dll DDR.exe --config ddr-11-us.conf %*
+inject ddrhook1.dll DDR.exe --config ddr-12-us.conf %*
