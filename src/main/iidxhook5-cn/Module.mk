f0f6e22bcb630ad1f6e6a88e79aa9cbca4915a95
Will Xyen <willxinc@users.noreply.github.com>
2022-06-23 23:42:05 -0700

    iidxhook: fix builds in cn versions

--- src/main/iidxhook5-cn/Module.mk
+++ src/main/iidxhook5-cn/Module.mk
@@ -2,12 +2,14 @@ avsdlls         += iidxhook5-cn
 
 ldflags_iidxhook5-cn   := \
     -liphlpapi \
+    -lws2_32 \
 
 deplibs_iidxhook5-cn   := \
     avs \
 
 libs_iidxhook5-cn      := \
     iidxhook-util \
+    eamio \
     ezusb-emu \
     ezusb2-emu \
     ezusb2-iidx-emu \
