df21747f4a9dfdd24a1f8745e65576353c00c01c
icex2 <djh.icex2@gmail.com>
2022-10-19 00:44:07 +0200

    doc bugfix: Fix incorrect COM port ID
    Must be 1 and not 0 for iidx. See also the api.md docs.

--- src/main/iidxhook8/config-io.c
+++ src/main/iidxhook8/config-io.c
@@ -21,7 +21,7 @@ void iidxhook8_config_io_init(struct cconfig *config)
         IIDXHOOK8_CONFIG_IO_DEFAULT_DISABLE_CARD_READER_EMU_VALUE,
         "Disable card reader emulation and enable usage of real card "
         "reader "
-        "hardware on COM0 (for games supporting slotted readers)");
+        "hardware on COM1 (for games supporting slotted readers)");
 
     cconfig_util_set_bool(
         config,
