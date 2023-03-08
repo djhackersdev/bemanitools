e7ecf93c9c07e068dcfe0aa7f85a73e87aaf0c70
icex2 <djh.icex2@gmail.com>
2023-03-08 17:48:44 +0100

    iidhxook-util: Expose settings_path as a configuration option

--- src/main/iidxhook-util/config-misc.h
+++ src/main/iidxhook-util/config-misc.h
@@ -1,11 +1,14 @@
 #ifndef IIDXHOOK_CONFIG_MISC_H
 #define IIDXHOOK_CONFIG_MISC_H
 
+#include <windows.h>
+
 #include "cconfig/cconfig.h"
 
 struct iidxhook_config_misc {
     bool disable_clock_set;
     bool rteffect_stub;
+    char settings_path[MAX_PATH];
 };
 
 void iidxhook_config_misc_init(struct cconfig *config);
