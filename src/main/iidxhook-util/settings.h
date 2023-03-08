7026f51d045f1f7c1293d4ab82b1a1d5dd54b897
icex2 <djh.icex2@gmail.com>
2023-03-08 17:47:06 +0100

    iidxhook-util: Add option to configure path for storing settings
    Additional flexibility to cover use-cases like storing this
    data on a separate partition which keeps the game data folder
    clean and read-only.

--- src/main/iidxhook-util/settings.h
+++ src/main/iidxhook-util/settings.h
@@ -4,11 +4,20 @@
 #include "hook/iohook.h"
 
 /**
- * Remaps the paths for the settings drives d:\, e:\ and f:\
- * to local folders e\ and f\.
- * Needed on IIDX 9th to Sirius.
+ * Remaps the paths for storing "settings data" that are usually stored on the
+ * drives d:\, e:\ and f:\ by the game. This defaults to the local folders
+ * d\, e\ and f\.
+ * 
+ * Required hook for 9th to Lincle.
  */
 void settings_hook_init(void);
+
+/**
+ * Change the path where settings file are stored.
+ * 
+ * @param path Destination path for storage, relative or absolute.
+ */
+void settings_hook_set_path(const char* path);
 
 /**
  * iohook dispatch function
