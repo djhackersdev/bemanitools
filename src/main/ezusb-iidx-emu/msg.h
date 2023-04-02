db8ff4e624187877babe778817ba0fab6826495a
icex2 <djh.icex2@gmail.com>
2023-04-03 01:09:44 +0200

    feat(iidx/ezusb): Make iidx ezusb emu io board type configurable
    Second part to fixing the 10th style SQ-INIT error. This has
    cross-impact on the kind of security data the game expects.
    Therefore, we need to expose the IO board type and make it
    configurable like the security data for the different game
    versions.
    
    More details explained in a dev journal entry to following
    in one of the next commits.

--- src/main/ezusb-iidx-emu/msg.h
+++ src/main/ezusb-iidx-emu/msg.h
@@ -8,16 +8,26 @@
 
 #include "ezusb-emu/msg.h"
 
+enum ezusb_iidx_emu_msg_io_board_type {
+    EZUSB_IIDX_EMU_MSG_IO_BOARD_TYPE_C02 = 0,
+    EZUSB_IIDX_EMU_MSG_IO_BOARD_TYPE_D01 = 1,
+    EZUSB_IIDX_EMU_MSG_IO_BOARD_TYPE_COUNT = 2,
+};
+
 /**
- * Init the fully emulated IIDX msg backend for a EZUSB (C02) board. This
+ * Init the fully emulated IIDX msg backend for a EZUSB board. This
  * activates the "old" V1 node handling backend which was used on iidx 09 to 13.
  * On iidx14, the firmware was updated and removed nodes that were not used
  * anymore, e.g. serial card reader handling.
  *
+ * @param io_board_type Type of IO board to emulate. Note that this impacts the
+ *        security data which is different on some game versions, e.g. 10th
+ *        style.
  * @return ezusb_emu_msg_hook structure with hook calls for ezusb msg
  *         dispatching
  */
-struct ezusb_emu_msg_hook *ezusb_iidx_emu_msg_init(void);
+struct ezusb_emu_msg_hook *ezusb_iidx_emu_msg_init(
+    enum ezusb_iidx_emu_msg_io_board_type io_board_type);
 
 /**
  * Init the fully emulated IIDX msg backend for a EZUSB (C02) board. This
