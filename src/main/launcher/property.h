b22322138194ac444e243b8140a31dfe722d7a50
Shiz <hi@shiz.me>
2023-04-30 19:29:15 +0200

    launcher: implement boot property parsing through AVS

--- src/main/launcher/property.h
+++ src/main/launcher/property.h
@@ -4,6 +4,7 @@
 #include "imports/avs.h"
 
 struct property *boot_property_load(const char *filename);
+struct property *boot_property_load_avs(const char *filename);
 struct property *boot_property_load_cstring(const char *cstring);
 void boot_property_free(struct property *prop);
 
