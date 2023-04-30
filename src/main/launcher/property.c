bc4db2b3c757ecae2f24fed77a409a01efe6b212
Shiz <hi@shiz.me>
2023-04-30 19:29:15 +0200

    launcher: implement boot property parsing through AVS

--- src/main/launcher/property.c
+++ src/main/launcher/property.c
@@ -11,6 +11,36 @@
 #include "util/log.h"
 #include "util/mem.h"
 
+typedef void (*rewinder)(uint32_t context);
+
+static struct property *do_property_load(
+    avs_reader_t reader, rewinder rewinder, uint32_t context, const char *name)
+{
+    struct property *prop;
+    void *buffer;
+    int nbytes;
+
+    nbytes = property_read_query_memsize(reader, context, 0, 0);
+
+    if (nbytes < 0) {
+        log_fatal("%s: Error querying configuration file", name);
+    }
+
+    buffer = xmalloc(nbytes);
+    prop = property_create(
+        PROPERTY_FLAG_READ | PROPERTY_FLAG_WRITE | PROPERTY_FLAG_CREATE |
+            PROPERTY_FLAG_APPEND,
+        buffer,
+        nbytes);
+    rewinder(context);
+
+    if (!property_insert_read(prop, 0, reader, context)) {
+        log_fatal("%s: Error reading configuration file", name);
+    }
+
+    return prop;
+}
+
 static int boot_property_fread(uint32_t context, void *bytes, size_t nbytes)
 {
     FILE *f;
@@ -18,6 +48,41 @@ static int boot_property_fread(uint32_t context, void *bytes, size_t nbytes)
     f = TlsGetValue(context);
 
     return fread(bytes, 1, nbytes, f);
+}
+
+static void boot_property_frewind(uint32_t context)
+{
+    FILE *f = TlsGetValue(context);
+    rewind(f);
+}
+
+struct property *boot_property_load(const char *filename)
+{
+    FILE *f;
+    uint32_t f_keyhole;
+    struct property *prop;
+
+    /* AVS callbacks are only given a 32-bit context parameter, even in 64-bit
+       builds of AVS. We allocate a 32-bit TLS key and pass the context in this
+       manner instead. Inefficient, but it works. */
+
+    f = fopen(filename, "r");
+
+    f_keyhole = TlsAlloc();
+    TlsSetValue(f_keyhole, f);
+
+    if (f == NULL) {
+        log_fatal("%s: Error opening configuration file", filename);
+    }
+
+    prop = do_property_load(
+        boot_property_fread, boot_property_frewind, f_keyhole, filename);
+
+    TlsFree(f_keyhole);
+
+    fclose(f);
+
+    return prop;
 }
 
 struct cstring_read_handle {
@@ -40,57 +105,16 @@ boot_property_cstring_read(uint32_t context, void *bytes, size_t nbytes)
     return result;
 }
 
-struct property *boot_property_load(const char *filename)
+static void boot_property_cstring_rewind(uint32_t context)
 {
-    struct property *prop;
-    void *buffer;
-    int nbytes;
-    FILE *f;
-    uint32_t f_keyhole;
-
-    /* AVS callbacks are only given a 32-bit context parameter, even in 64-bit
-       builds of AVS. We allocate a 32-bit TLS key and pass the context in this
-       manner instead. Inefficient, but it works. */
-
-    f = fopen(filename, "r");
-
-    f_keyhole = TlsAlloc();
-    TlsSetValue(f_keyhole, f);
-
-    if (f == NULL) {
-        log_fatal("%s: Error opening configuration file", filename);
-    }
-
-    nbytes = property_read_query_memsize(boot_property_fread, f_keyhole, 0, 0);
-
-    if (nbytes < 0) {
-        log_fatal("%s: Error querying configuration file", filename);
-    }
-
-    buffer = xmalloc(nbytes);
-    prop = property_create(
-        PROPERTY_FLAG_READ | PROPERTY_FLAG_WRITE | PROPERTY_FLAG_CREATE |
-            PROPERTY_FLAG_APPEND,
-        buffer,
-        nbytes);
-    rewind(f);
-
-    if (!property_insert_read(prop, 0, boot_property_fread, f_keyhole)) {
-        log_fatal("%s: Error reading configuration file", filename);
-    }
-
-    TlsFree(f_keyhole);
-
-    fclose(f);
-
-    return prop;
+    struct cstring_read_handle *h = TlsGetValue(context);
+    h->offset = 0;
 }
+
 struct property *boot_property_load_cstring(const char *cstring)
 {
-    struct property *prop;
-    void *buffer;
-    int nbytes;
     uint32_t s_keyhole;
+    struct property *prop;
 
     // see above
     struct cstring_read_handle read_handle;
@@ -101,26 +125,43 @@ struct property *boot_property_load_cstring(const char *cstring)
     s_keyhole = TlsAlloc();
     TlsSetValue(s_keyhole, &read_handle);
 
-    nbytes = property_read_query_memsize(
-        boot_property_cstring_read, s_keyhole, 0, 0);
-
-    if (nbytes < 0) {
-        log_fatal("Error querying configuration string");
-    }
-
-    buffer = xmalloc(nbytes);
-    prop = property_create(
-        PROPERTY_FLAG_READ | PROPERTY_FLAG_WRITE | PROPERTY_FLAG_CREATE |
-            PROPERTY_FLAG_APPEND,
-        buffer,
-        nbytes);
-
-    read_handle.offset = 0;
-    if (!property_insert_read(prop, 0, boot_property_cstring_read, s_keyhole)) {
-        log_fatal("Error inserting configuration string");
-    }
+    prop = do_property_load(
+        boot_property_cstring_read,
+        boot_property_cstring_rewind,
+        s_keyhole,
+        "<string>");
 
     TlsFree(s_keyhole);
+
+    return prop;
+}
+
+static int boot_property_avs_read(uint32_t context, void *bytes, size_t nbytes)
+{
+    avs_desc desc = (avs_desc) context;
+    return avs_fs_read(desc, bytes, nbytes);
+}
+
+static void boot_property_avs_rewind(uint32_t context)
+{
+    avs_desc desc = (avs_desc) context;
+    avs_fs_lseek(desc, 0, AVS_SEEK_SET);
+}
+
+struct property *boot_property_load_avs(const char *filename)
+{
+    avs_desc desc;
+    struct property *prop;
+
+    desc = avs_fs_open(filename, AVS_FILE_READ, AVS_FILE_FLAG_SHARE_READ);
+    if (!desc) {
+        log_fatal("%s: Error opening configuration file", filename);
+    }
+
+    prop = do_property_load(
+        boot_property_avs_read, boot_property_avs_rewind, desc, filename);
+
+    avs_fs_close(desc);
 
     return prop;
 }
