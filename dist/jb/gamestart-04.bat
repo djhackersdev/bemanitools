82343d8cf1a2b5cf3c11dfedc669625b21559b79
Will Toohey <will@mon.im>
2023-10-18 09:24:58 +1000

    Fix missing xml for jb04. Fixes booting Ave.

--- dist/jb/gamestart-04.bat
+++ dist/jb/gamestart-04.bat
@@ -3,6 +3,7 @@
 cd /d %~dp0
 
 if not exist dev\nvram mkdir dev\nvram
+if not exist dev\nvram\ea3-config.xml copy prop\ea3-config.xml dev\nvram\ea3-config.xml
 if not exist dev\nvram\coin.xml copy prop\defaults\coin.xml dev\nvram\coin.xml
 if not exist dev\nvram\eacoin.xml copy prop\defaults\eacoin.xml dev\nvram\eacoin.xml
 if not exist dev\raw mkdir dev\raw
