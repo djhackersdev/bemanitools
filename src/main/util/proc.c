9f8fed6fa96ab0c6b7ece512e192664156a7f1a0
icex2 <djh.icex2@gmail.com>
2024-01-28 21:58:24 +0100

    feat(util): Add func to check if running as admin user
    This has been a source of common error in the past.
    It is known that most, or even all, games run into
    various issues when not run with elevated privileges.

--- src/main/util/proc.c
+++ src/main/util/proc.c
@@ -1,6 +1,54 @@
-#include <windows.h>
+#define LOG_MODULE "util-proc"
 
+#include <windows.h>
+#include <winnt.h>
+
+#include <stdbool.h>
 #include <stdint.h>
+
+#include "util/log.h"
+
+bool proc_is_running_as_admin_user()
+{
+    SID_IDENTIFIER_AUTHORITY authority = SECURITY_NT_AUTHORITY;
+    PSID sid;
+    BOOL res;
+    BOOL is_admin;
+
+    res = AllocateAndInitializeSid(
+        &authority,
+        2,
+        SECURITY_BUILTIN_DOMAIN_RID,
+        DOMAIN_ALIAS_RID_ADMINS,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        &sid);
+
+    if (!res) {
+        log_warning(
+            "Failed to allocate memory for is admin check: %lX",
+            GetLastError());
+        return false;
+    }
+
+    is_admin = false;
+
+    res = CheckTokenMembership(NULL, sid, &is_admin);
+
+    if (!res) {
+        log_warning(
+            "Failed to check admin group membership: %lX", GetLastError());
+        return false;
+    }
+
+    FreeSid(sid);
+
+    return is_admin;
+}
 
 void proc_terminate_current_process(uint32_t exit_code)
 {
