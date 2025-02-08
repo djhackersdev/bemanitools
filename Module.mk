50aa6226a37e505b0e8ef7f5a3858596b5612f65
icex2 <icex2@users.noreply.github.com>
2025-02-08 23:22:46 +0100

    feat: Add separate d3d9-frame-graph-hook (#321)
    Allow this to be hooked by any application with a d3d9
    render backend. This is not compatible with d3d9ex.

--- Module.mk
+++ Module.mk
@@ -100,6 +100,7 @@ include src/main/bstio/Module.mk
 include src/main/camhook/Module.mk
 include src/main/cconfig/Module.mk
 include src/main/config/Module.mk
+include src/main/d3d9-frame-graph-hook/Module.mk
 include src/main/d3d9-util/Module.mk
 include src/main/d3d9exhook/Module.mk
 include src/main/ddrhook-util/Module.mk
@@ -239,6 +240,7 @@ $(zipdir)/tools.zip: \
 		build/bin/indep-32/ezusb2-tool.exe \
 		build/bin/indep-32/ezusb-tool.exe \
 		build/bin/indep-32/nvgpu.exe \
+		build/bin/indep-32/d3d9-frame-graph-hook.dll \
 		| $(zipdir)/
 	$(V)echo ... $@
 	$(V)zip -j $@ $^
@@ -254,6 +256,7 @@ $(zipdir)/tools-x64.zip: \
 		build/bin/indep-64/jbiotest.exe \
 		build/bin/indep-64/mempatch-hook.dll \
 		build/bin/indep-64/nvgpu.exe \
+		build/bin/indep-64/d3d9-frame-graph-hook.dll \
 		| $(zipdir)/
 	$(V)echo ... $@
 	$(V)zip -j $@ $^
