9e2319bd136fe738d0b7ee5ec950ea5c6b8e6947
icex2 <djh.icex2@gmail.com>
2025-02-11 15:34:38 +0100

    feat: Bump iidxhook9 supported versions, EPOLIS “support"
    No changes required, just make support visible by reflecting
    it in distribution package naming, documentation and
    filenaming

--- Module.mk
+++ Module.mk
@@ -431,7 +431,7 @@ $(zipdir)/iidx-25-to-26.zip: \
 	$(V)echo ... $@
 	$(V)zip -j $@ $^
 
-$(zipdir)/iidx-27-to-30.zip: \
+$(zipdir)/iidx-27-to-31.zip: \
 		build/bin/avs2_1700-64/iidxhook9.dll \
 		build/bin/avs2_1700-64/launcher.exe \
 		build/bin/indep-64/config.exe \
@@ -444,10 +444,12 @@ $(zipdir)/iidx-27-to-30.zip: \
 		dist/iidx/gamestart-28.bat \
 		dist/iidx/gamestart-29.bat \
 		dist/iidx/gamestart-30.bat \
+		dist/iidx/gamestart-31.bat \
 		dist/iidx/iidxhook-27.conf \
 		dist/iidx/iidxhook-28.conf \
 		dist/iidx/iidxhook-29.conf \
 		dist/iidx/iidxhook-30.conf \
+		dist/iidx/iidxhook-31.conf \
 		dist/iidx/vefx.txt \
 		| $(zipdir)/
 	$(V)echo ... $@
@@ -847,7 +849,7 @@ $(BUILDDIR)/bemanitools.zip: \
 		$(zipdir)/iidx-20-cn.zip \
 		$(zipdir)/iidx-21-to-24.zip \
 		$(zipdir)/iidx-25-to-26.zip \
-		$(zipdir)/iidx-27-to-30.zip \
+		$(zipdir)/iidx-27-to-31.zip \
 		$(zipdir)/iidx-hwio-x86.zip \
 		$(zipdir)/iidx-hwio-x64.zip \
 		$(zipdir)/jb-01.zip \
