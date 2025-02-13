c0be02a24f10ae58ba31f314fdfae8f1fa01e280
icex2 <icex2@users.noreply.github.com>
2025-02-13 14:28:34 +0100

    feat: d3d9-monitor-check tool (#322)
    A separate application to run the infamous IIDX “monitor check”.
    
    The on-screen output has been enhanced to provide more
    detailed information about the on-going process. The tool
    furthermore allows basic querying of adapter and adapter
    modes information from the command line. These additional
    options can be useful to gather more debug information
    or select correct parameters for the monitor test from actually
    supported mode parameters by the adapter.
    
    The tool has been tested on an actual cabinet with nvgpu
    setting different custom timings. The accuracy seems to be
    even higher than what IIDX’s monitor check is actually showing.
    For example, with a custom timing of 59.900, the tool yields 59.902.
    Meanwhile, the monitor check of iidx 29 59.8981 hz to 59.8997 hz
    on screen.
    
    This doesn’t mean the game’s getting inaccurate values. The game
    actually syncs and plays just fine. However, the game’s monitor check
    cannot be used as a reference for “getting the avg. refresh rate”
    value as an input parameter for other tools, e.g. chart patching with
    btools.
    
    Co-authored-by: icex2 <djh.icex2@gmail.com>

--- Module.mk
+++ Module.mk
@@ -100,6 +100,8 @@ include src/main/bstio/Module.mk
 include src/main/camhook/Module.mk
 include src/main/cconfig/Module.mk
 include src/main/config/Module.mk
+include src/main/d3d9-frame-graph-hook/Module.mk
+include src/main/d3d9-monitor-check/Module.mk
 include src/main/d3d9-util/Module.mk
 include src/main/d3d9exhook/Module.mk
 include src/main/ddrhook-util/Module.mk
@@ -232,6 +234,9 @@ $(zipdir)/tools.zip: \
 		build/bin/indep-32/ezusb2-dbg-hook.dll \
 		build/bin/indep-32/ezusb2-tool.exe \
 		build/bin/indep-32/ezusb-tool.exe \
+		build/bin/indep-32/nvgpu.exe \
+		build/bin/indep-32/d3d9-frame-graph-hook.dll \
+		build/bin/indep-32/d3d9-monitor-check.exe \
 		| $(zipdir)/
 	$(V)echo ... $@
 	$(V)zip -j $@ $^
@@ -246,6 +251,9 @@ $(zipdir)/tools-x64.zip: \
 		build/bin/indep-64/iidx-ezusb2-exit-hook.dll \
 		build/bin/indep-64/jbiotest.exe \
 		build/bin/indep-64/mempatch-hook.dll \
+		build/bin/indep-64/nvgpu.exe \
+		build/bin/indep-64/d3d9-frame-graph-hook.dll \
+		build/bin/indep-64/d3d9-monitor-check.exe \
 		| $(zipdir)/
 	$(V)echo ... $@
 	$(V)zip -j $@ $^
